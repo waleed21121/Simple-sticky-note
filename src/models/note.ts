ca6aa5f9cdb83619fa0f9c93276244c61febfd02
waleed <wa581509@gmail.com>
2024-06-30 00:03:53 +0300

    initial commit

--- src/models/note.ts
+++ src/models/note.ts
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const noteSchema = new mongoose.Schema({
     contents: {
@@ -9,4 +9,4 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-module.exports = Note;
+export default Note;
